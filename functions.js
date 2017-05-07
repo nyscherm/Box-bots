00f533aeed5857b9384f06d2b13e9326e8c64ebc
nyscherm <nyscherm@gmail.com>
2017-05-07 11:02:18 -0700

    clean up bugs with touch events

--- functions.js
+++ functions.js
@@ -36,46 +36,49 @@ $(document).ready(function(){
 
 	//socket.send(JSON.stringify(demo);
 
-	$(".joystick").bind('mousedown touchstart', function(event) {
+	$(".joystick").bind('touchstart', function(event) {
 		$(".coordinates").removeClass("hidden");
 	    $(".stem").removeClass("hidden");
 	    startTracking(this, "joystick");
-    }).bind('mouseup touchend', function(event) {
+    }).bind('touchend', function(event) {
     	$(".coordinates").addClass("hidden");
     	$(".stem").addClass("hidden");
     	$(".joystick .handle").css("transform", "translate(0,0)");
     	stopTracking(this);
 	});
 
-	$(".sensitivity .slider").bind('mousedown touchstart', function(event) {
+	$(".sensitivity .slider").bind('touchstart', function(event) {
 	    startTracking(this, "slider");
-    }).bind('mouseup touchend', function(event) {
+    }).bind('touchend', function(event) {
     	stopTracking(this);
     	savedSettings.sliderPos = sliderPos;
 	});
 
-	$(".indicator").bind('mousedown touchstart', function(event) {
+	$(".indicator").bind('touchstart', function(event) {
 	    startTracking(this, "indicator");
-    }).bind('mouseup touchend', function(event) {
+    }).bind('touchend', function(event) {
     	stopTracking(this);
 		savedSettings.trimPos = indicatorPos;
 	});
 
 	function startTracking($this, target){
-		xStart = event.pageX;
-		yStart = event.pageY;
+		xStart = event.touches[0].pageX;
+		yStart = event.touches[0].pageY;
+		console.log(event);
+		console.log(xStart);
+		console.log(yStart);
 		
 		$($this).on("touchmove", function() {
 	        //console.log("OK Moved!");
 			if (target == "joystick") {
-		        posX = Math.max(Math.min(event.pageX - xStart, 50), -50);
-				posY = Math.max(Math.min(event.pageY - yStart, 50), -50);
+		        posX = Math.max(Math.min(event.touches[0].pageX - xStart, 50), -50);
+				posY = Math.max(Math.min(event.touches[0].pageY - yStart, 50), -50);
 				var msg = 'touchmove() position - x : ' + posX + ', y : ' + posY;
 			  	$(".coordinates").text(msg);
 			    $(".joystick .handle").css("transform", "translate("+posX+"px, "+posY+ "px)");
 			}
 			else if (target == "slider") {
-				posX = event.pageX - xStart;
+				posX = event.touches[0].pageX - xStart;
 				sliderPos = Math.max(Math.min(posX + sliderPos, 250), 5);
 				$(".slider .bar").css("width", 10 + sliderPos + "px");
 				var msg = sliderPos*2/5 + '%';
@@ -83,7 +86,7 @@ $(document).ready(function(){
 			  	console.log(sliderPos);
 			}
 			else if (target == "indicator") {
-				posX = event.pageX - xStart;
+				posX = event.touches[0].pageX - xStart;
 				indicatorPos = Math.max(Math.min(indicatorPos + posX, 100), 1);
 				$(".indicator").css("margin-left", indicatorPos*.75 + "%");
 				var msg = ((indicatorPos-50)/50).toFixed(2);
